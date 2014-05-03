2ad1d49cdf1c225912eba70de3c72228651b1fd7
vyrp <felipe.croata@gmail.com>
2014-05-03 02:35:26 +0000

    Jinja templates

--- server/index.py
+++ server/index.py
@@ -5,7 +5,27 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    users = [
+        {
+            "rank": 1,
+            "username": 'vyrp',
+            "problems": 100,
+            "score": 40.30,
+        },
+        {
+            "rank": 2,
+            "username": 'harry',
+            "problems": 90,
+            "score": 35.35,
+        },
+        {
+            "rank": 3,
+            "username": 'h4x0r',
+            "problems": 85,
+            "score": 33.67,
+        },
+    ]
+    return render_template("index.html", users=users)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
