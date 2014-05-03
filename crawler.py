8902572810b7f10a68ce79b170e2f1d2bbe61d53
Filipe <luizmramos@gmail.com>
2014-05-02 22:37:39 -0300

    Adding thread pool.

--- crawler.py
+++ crawler.py
@@ -1,8 +1,42 @@
 import urllib2
 import re
+import multiprocessing.pool
+from Queue import Queue
+from threading import Thread
+import thread
+
+class Worker(Thread):
+    """Thread executing tasks from a given tasks queue"""
+    def __init__(self, tasks):
+        Thread.__init__(self)
+        self.tasks = tasks
+        self.daemon = True
+        self.start()
+    
+    def run(self):
+        while True:
+            func, args, kargs = self.tasks.get()
+            try: func(*args, **kargs)
+            except Exception, e: print e
+            self.tasks.task_done()
+
+class ThreadPool:
+    """Pool of threads consuming tasks from a queue"""
+    def __init__(self, num_threads):
+        self.tasks = Queue(num_threads)
+        for _ in range(num_threads): Worker(self.tasks)
+
+    def add_task(self, func, *args, **kargs):
+        """Add a task to the queue"""
+        self.tasks.put((func, args, kargs))
+
+    def wait_completion(self):
+        """Wait for completion of all the tasks in the queue"""
+        self.tasks.join()
 
 problem_regex = re.compile(r'<a href=\"\/problems\/([A-Z0-9]*)\/\"')
 finished_regex = re.compile(r'<i>Pr&#243;ximo<\/i>')
+
 
 def getProblems(line):
 	it = problem_regex.finditer(line)
@@ -14,8 +48,15 @@ def getProblems(line):
 def hasFinished(line):
 	return len(finished_regex.findall(line)) != 0
 
-def crawl_problems():
+
+def crawl_problem(problem, lock):
+	with lock:
+		print problem
+
+def crawl_all_problems():
+	pool = ThreadPool(10)
 	problem_sets = ["contest_noturno", "mineira", "obi", "regionais", "seletivas", "seletiva_ioi", 	"sulamericana"]	
+	lock = thread.allocate_lock()
 	for problem_set in problem_sets:
 		offset = 0	
 		while True:
@@ -23,11 +64,11 @@ def crawl_problems():
 			html = urllib2.urlopen(problem_set_url).read()
 			problems = getProblems(html)
 			for problem in problems:
-				print problem
+				pool.add_task(crawl_problem, problem, lock)
 			offset = offset + len(problems)
 			if hasFinished(html):
 				break
 
 if __name__ == "__main__":
-	crawl_problems()
+	crawl_all_problems()
 	
