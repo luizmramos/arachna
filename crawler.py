fb9eb1583e2314703bf9207f23a8e494abc01e14
Filipe <luizmramos@gmail.com>
2014-05-02 22:03:53 -0300

    Crawling problems

--- crawler.py
+++ crawler.py
@@ -1,17 +1,33 @@
 import urllib2
 import re
 
-
-problem_regex = re.compile(r'<a href=\"\/problems\/.*\"')
+problem_regex = re.compile(r'<a href=\"\/problems\/([A-Z0-9]*)\/\"')
+finished_regex = re.compile(r'<i>Pr&#243;ximo<\/i>')
 
 def getProblems(line):
-	return problem_regex.findall(line)
+	it = problem_regex.finditer(line)
+	all_problems = []
+	for problem in it:
+		all_problems.append(problem.group(1))
+	return all_problems
 
+def hasFinished(line):
+	return len(finished_regex.findall(line)) != 0
 
+def crawl_problems():
+	problem_sets = ["contest_noturno", "mineira", "obi", "regionais", "seletivas", "seletiva_ioi", 	"sulamericana"]	
+	for problem_set in problem_sets:
+		offset = 0	
+		while True:
+			problem_set_url = "http://br.spoj.com/problems/" + problem_set + "/start=" + str(offset)
+			html = urllib2.urlopen(problem_set_url).read()
+			problems = getProblems(html)
+			for problem in problems:
+				print problem
+			offset = offset + len(problems)
+			if hasFinished(html):
+				break
 
-problem_sets = ["contest_noturno", "mineira", "obi", "regionais", "seletivas", "seletiva_ioi", 	"sulamericana"]
-
-for problem_set in problem_sets:
-	problems = getProblems(urllib2.urlopen("http://br.spoj.com/problems/" + problem_set).read())
-	for problem in problems:
-		print problem
+if __name__ == "__main__":
+	crawl_problems()
+	
