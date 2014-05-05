e77e05c9f3a60c7c13e54116512ee75b545215b5
vyrp <felipe.croata@gmail.com>
2014-05-05 23:14:12 +0000

    logging downs and sweeping old crawler logs

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-[ "`service arachna status | grep -c not`" = 1 ] && sudo service arachna start
+[ "`service arachna status | grep -c not`" = 1 ] && sudo service arachna start && echo "`date +%Y%m%d%H%M`" >> /Arachna/logs/server/down.log
--- scripts/crawler.sh
+++ scripts/crawler.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 /usr/bin/python /Arachna/crawler/crawler.py &> /Arachna/logs/crawler/`date +%Y%m%d%H`.log
+find /Arachna/logs/crawler/* -mtime +7 -exec rm {} \;
