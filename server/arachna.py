28a4c768d5e0e1caf5873e2107cd1b811adbd245
vyrp <felipe.croata@gmail.com>
2014-05-05 02:31:14 +0000

    500 server error

--- server/arachna.py
+++ server/arachna.py
@@ -36,52 +36,45 @@ def index():
 def search(type):
     if not (type == "score" or type == "problems"):
         type = "score"
-    try:
-        user = request.form["user"]
-        users = convert(get_all(type), 1)
-        amount = get_amount()
-        filtered = filter(lambda u: u["username"] == user, users)
-        
-        if filtered:
-            rank = filtered[0]["rank"]
-            page = (rank-1) / 1000 + 1
-        else:
-            rank = -1
-            page = 1
 
-        users_list = get_all_by(type, page)
-        template = render_template("search.html", users=convert(users_list, page), amount=amount, page=page, rank=rank, url=type)
-    except Exception as e:
-        print e
-    return template
+    user = request.form["user"]
+    users = convert(get_all(type), 1)
+    amount = get_amount()
+    filtered = filter(lambda u: u["username"] == user, users)
+    
+    if filtered:
+        rank = filtered[0]["rank"]
+        page = (rank-1) / 1000 + 1
+    else:
+        rank = -1
+        page = 1
+
+    users_list = get_all_by(type, page)
+    return render_template("search.html", users=convert(users_list, page), amount=amount, page=page, rank=rank, url=type)
 
 @app.route("/score/")
 @app.route("/score/<int:page>")
 def score(page=1):
-    try:
-        amount = get_amount()
-        page = page if page <= amount else amount
-        users_list = get_all_by("score", page)
-        template = render_template("rank.html", users=convert(users_list, page), amount=amount, page=page, url="score")
-    except Exception as e:
-        print e
-    return template
+    amount = get_amount()
+    page = page if page <= amount else amount
+    users_list = get_all_by("score", page)
+    return render_template("rank.html", users=convert(users_list, page), amount=amount, page=page, url="score")
 
 @app.route("/problems/")
 @app.route("/problems/<int:page>")
 def problems(page=1):
-    try:
-        amount = get_amount()
-        page = page if page <= amount else amount
-        users_list = get_all_by("problems", page)
-        template = render_template("rank.html", users=convert(users_list, page), amount=amount, page=page, url="problems")
-    except Exception as e:
-        print e
-    return template
+    amount = get_amount()
+    page = page if page <= amount else amount
+    users_list = get_all_by("problems", page)
+    return render_template("rank.html", users=convert(users_list, page), amount=amount, page=page, url="problems")
 
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template("404.html"), 404
+
+@app.errorhandler(500)
+def server_error(error):
+    return render_template("500.html"), 500
 
 def signal_handler(signal, frame):
     logger.warn("=== Process stopped (%d) ===" % signal)
